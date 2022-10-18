9e4b749f840a6637ed4280e77d704178ba78ed7b
Simon Staehli <simon@DESKTOP-6EDHCK5.localdomain>
2022-10-18 13:53:02 +0200

    download func, path extraction

--- bucket/bucket.py
+++ bucket/bucket.py
@@ -9,12 +9,14 @@ This script defines an API endpoint to load data from a S3 bucket.
 import os
 import sys
 import yaml
+import time
 
 from flask import Flask
 from flask import request, jsonify
 
 from minio import Minio
-from minio.commonconfig import Tags
+
+from multiprocessing.pool import ThreadPool as Pool
 
 
 app = Flask(__name__)
@@ -34,7 +36,7 @@ client = Minio(
 #os.environ[]
 
 
-def extract_sub_prefix(client: object, bucket_name: str, max_depth: int = 2, current_objects: dict = {}, recursive_depth: int = 0):
+def extract_sub_prefix(client: object, bucket_name: str, max_depth: int = 2, current_objects: dict = {}, recursive_depth: int = 0) -> dict:
     """
     Extract subfolders for one s3 bucket.
 
@@ -89,6 +91,89 @@ def extract_sub_prefix(client: object, bucket_name: str, max_depth: int = 2, cur
                 recursive_depth=recursive_depth + 1
             )
     return current_objects
+
+
+def get_object_paths(client: object, bucket_name: str, prefix: str | list, file_endings: list = ['.jpg', '.png']) -> list:
+    """
+    Extracts the path of all objects in a bucket by given prefix.
+
+    Parameters
+    ----------
+    client : object
+        Minio s3 client
+
+    bucket_name : str
+        Name of the s3 storage bucket
+
+    prefix : str | list
+        Prefix to look through for relevant paths. Can be either only one or multiple paths.
+
+    file_endings : list, optional
+        Filter objects paths by file ending, by default ['.jpg', '.png']
+
+    Returns
+    -------
+    list
+        List of all object paths in the bucket and prefix.
+    """
+    object_paths = []
+    if isinstance(prefix, list):
+        for p in prefix:        
+            for element in client.list_objects(bucket_name=bucket_name, prefix=p, recursive=True):
+                if any([element.object_name.endswith(f_suffix) for f_suffix in file_endings]):
+                    object_paths.append(element.object_name)
+    else:
+        for element in client.list_objects(bucket_name=bucket_name, prefix=prefix, recursive=True):
+                object_paths.append(element.object_name)
+
+    return object_paths
+
+
+def download_files(client: object, bucket_name: str, filenames: list, n_threads: int = 8):
+    """
+    Downloads all files given by path. Simultaneously creates similar folder structure local.
+
+    Parameters
+    ----------
+    client : object
+        Initiated minio client for s3 storage
+
+    bucket_name : str
+        name of the bucket
+
+    filenames : list
+        filenames to extract from bucket
+
+    n_threads : int, optional
+        number of threads to use for download, by default 8
+    """
+    # Create equal data structure in local repo
+    path_dirs = [os.path.split(f)[0] for f in filenames]
+    for new_dir in set(path_dirs):
+        try:
+            os.makedirs(new_dir)
+        except FileExistsError as fe:
+            print(fe)
+
+    # Download an object
+    def _download_file_mp(filename: str):
+        print(f'Loading {filename}')
+        try:
+            client.fget_object(
+                bucket_name=bucket_name, 
+                object_name=filename, 
+                file_path=filename
+            )
+        except Exception as e:
+            print(e, f'Not worked for {filename}')
+
+    start = time.perf_counter()
+    # Use multiprocessing (or multithreading?)
+    with Pool(processes=n_threads) as pool:
+        pool.starmap(_download_file_mp, list(zip(filenames)))    
+    end = time.perf_counter() - start
+
+    print(f'Extracted {len(filenames)} files in {end} seconds')
 
 
 def get_bucket_data(client: object, bucket: str, prefix: str = None) -> object:
