2569aa5694aa65dcbe3b9bd66f249e4e5f54504c
Simon.Staehli <simon.a.staehli@gmail.com>
2022-11-04 15:14:03 +0100

    add image results ingestion task

--- src/etl/load.py
+++ src/etl/load.py
@@ -28,8 +28,8 @@ def is_model_config_up(conn: object, model_config: dict) -> bool:
     bool
         Returns True if the configurations is already up else False
     """
-    with conn.cursor() as cursor:
-        try:        
+    try:
+        with conn.cursor() as cursor:        
             cursor.execute(
             """
             SELECT configuration FROM pollinator_inference_config
@@ -38,10 +38,10 @@ def is_model_config_up(conn: object, model_config: dict) -> bool:
             all_configs = cursor.fetchall()
             if len(all_configs) > 0:
                 all_configs = [element[0] for element in all_configs]  
-        except Exception:
-            raise Exception('Could not request configuration')
-        finally:
-            conn.commit()      
+    except Exception:
+        raise Exception('Could not request configuration')
+    finally:
+        conn.commit()      
     
     return model_config in all_configs
 
@@ -60,8 +60,8 @@ def db_insert_model_config(conn: object, model_config: dict):
     if not is_model_config_up(conn=conn, model_config=model_config):
         # upload current model config to DB as json
         model_config_json = json.dumps(model_config)
-        with conn.cursor() as cursor:
-            try:    
+        try:    
+            with conn.cursor() as cursor:
                 cursor.execute(
                     f"""
                     INSERT INTO pollinator_inference_config (config_id, configuration)
@@ -69,17 +69,52 @@ def db_insert_model_config(conn: object, model_config: dict):
                     """,
                     (model_config['config_id'], model_config_json)
                 )
-            except Exception:
-                raise Exception('Could not insert.')
-            finally:
-                conn.commit()
+        except Exception:
+            raise Exception('Could not insert.')
+        finally:
+            conn.commit()
     else:
         print('Model Config alrealdy up.')
 
+@task
+def db_insert_image_results(conn: object, data: pd.DataFrame, model_config: dict):
+    """Adds processed data to image_results. Insert statement looks duplicated values during insertion.
 
+    Parameters
+    ----------
+    conn : object
+        psycopg connection object
 
-def db_insert_image_results(cursor: object, files: list | pd.DataFrame):
-    pass
+    data : pd.DataFrame
+        dataframe where image_results are stored
+
+    model_config : dict
+        model configuration
+
+    """
+    model_id = model_config['config_id']
+    data['model_id'] = model_id
+    records = data[['file_id', 'model_id']].to_records(index=False)
+    try:    
+        with conn.cursor() as cursor:
+            for record in records:  
+                file_id = int(record[0])
+                model_id = record[1]
+                cursor.execute(
+                    """
+                    INSERT INTO image_results (file_id, config_id)
+                    SELECT %s, %s
+                    WHERE NOT EXISTS (
+                        SELECT file_id, config_id FROM image_results 
+                        WHERE file_id = %s AND config_id = %s
+                    )
+                    """,
+                    (file_id, model_id, file_id, model_id)
+                )
+    except Exception:
+        raise Exception('Could not insert.')
+    finally:
+        conn.commit()
 
 
 
