7f49dba183f16486f8926402b7db550cfd59c82e
Simon.Staehli <simon.a.staehli@gmail.com>
2022-11-04 12:17:22 +0100

    add sql for modelconfig

--- src/etl/load.py
+++ src/etl/load.py
@@ -2,28 +2,48 @@
 This python file includes all task regarding to loading the data from the predictions
 to the database and updating all relevant columns with processed information
 """
-from prefect import task
-from concurrent.futures import process
 import pandas as pd
-from sqlalchemy import create_engine
+import psycopg2
+from prefect import task
+
+def is_model_config_up(conn: object, model_config: dict) -> bool:
+    with conn.cursor() as cursor:    
+        cursor.execute(
+        """
+        SELECT configuration FROM pollinator_inference_config
+        """
+        )
+        all_configs = cursor.fetchall()
+        all_configs = [element[0] for element in all_configs]        
+    
+    return model_config in all_configs
+
+def db_insert_model_config(conn: object, model_config: dict):
+    if not is_model_config_up(conn=conn, model_config=model_config):
+        # upload current model config to DB as json
+        model_config_json = json.dumps(model_config)
+        with conn.cursor() as cursor:
+            try:    
+                cursor.execute(
+                    f"""
+                    INSERT INTO pollinator_inference_config (config_id, configuration)
+                    VALUES (%s, %s)
+                    """,
+                    (model_config['config_id'], model_config_json)
+                )
+            except Exception:
+                raise Exception('Could not insert.')
+            finally:
+                conn.commit()
+    else:
+        print('Model Config alrealdy up.')
 
 
 
-def connect_db(user: str, password: str, hostname: str, database_name: str):
-    engine = create_engine('postgresql+psycopg2://{}:{}@{}/{}'.format(
-        user,
-        password,
-        hostname,
-        database_name
-    ))
-    return engine
-
-
-def upload_predictions(data: pd.DataFrame):
+def db_insert_image_results(cursor: object, files: list | pd.DataFrame):
     pass
 
-def alter_table_processed(data: pd.DataFrame):
-    pass
+
 
 
 @task
