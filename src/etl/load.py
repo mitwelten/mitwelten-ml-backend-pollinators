a09469009c602370c0bc98cf85f9b55688b77186
Simon.Staehli <simon.a.staehli@gmail.com>
2022-11-04 12:21:36 +0100

    add docstrings

--- src/etl/load.py
+++ src/etl/load.py
@@ -2,23 +2,61 @@
 This python file includes all task regarding to loading the data from the predictions
 to the database and updating all relevant columns with processed information
 """
+import json
+
 import pandas as pd
+
 import psycopg2
+
 from prefect import task
 
+
+
 def is_model_config_up(conn: object, model_config: dict) -> bool:
-    with conn.cursor() as cursor:    
-        cursor.execute(
-        """
-        SELECT configuration FROM pollinator_inference_config
-        """
-        )
-        all_configs = cursor.fetchall()
-        all_configs = [element[0] for element in all_configs]        
+    """Checks if model config was already uploaded to database.
+
+    Parameters
+    ----------
+    conn : object
+        psycopg connection object.
+
+    model_config : dict
+        model configurations
+
+    Returns
+    -------
+    bool
+        Returns True if the configurations is already up else False
+    """
+    with conn.cursor() as cursor:
+        try:        
+            cursor.execute(
+            """
+            SELECT configuration FROM pollinator_inference_config
+            """
+            )
+            all_configs = cursor.fetchall()
+            if len(all_configs) > 0:
+                all_configs = [element[0] for element in all_configs]  
+        except Exception:
+            raise Exception('Could not request configuration')
+        finally:
+            conn.commit()      
     
     return model_config in all_configs
 
+@task
 def db_insert_model_config(conn: object, model_config: dict):
+    """Inserts model config into db
+
+    Parameters
+    ----------
+    conn : object
+        psycopg connection object
+
+    model_config : dict
+        model configurations
+    """ 
     if not is_model_config_up(conn=conn, model_config=model_config):
         # upload current model config to DB as json
         model_config_json = json.dumps(model_config)
