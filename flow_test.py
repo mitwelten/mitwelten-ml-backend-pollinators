eaea3722610ad96ff90aa7a04a515ea54a91e451
Simon.Staehli <simon.a.staehli@gmail.com>
2022-11-10 11:46:45 +0100

    organize flow

--- flow_test.py
+++ flow_test.py
@@ -7,28 +7,30 @@ import pandas as pd
 from prefect import flow
 
 from src.etl.extract import (
-    download_files, 
+    download_files,
     get_checkpoint,
 )
 from src.etl.transform import (
-    model_predict, 
+    model_predict,
     process_flower_predictions,
     process_pollinator_predictions
 )
 
 from src.etl.load import (
-    update_processed_data, 
-    db_get_image_results, 
     db_insert_flower_predictions,
     db_insert_image_results,
-    db_insert_model_config
+    db_insert_model_config,
+    db_insert_pollinator_predictions
 )
-from src.etl.clients import get_db_client, get_minio_client
-
+from src.etl.clients import (
+    get_db_client,
+    get_minio_client
+)
 
 
 @flow(name='test-flow')
-def etl_flow(BATCHSIZE=32, CONFIG_PATH='test_config.yaml', MODEL_CONFIG_PATH = 'model_config.json', IS_TEST=True):
+def etl_flow(
+        BATCHSIZE=4, CONFIG_PATH='test_config.yaml', MODEL_CONFIG_PATH='model_config.json', IS_TEST=False, MULTI_RESULTS_FOR_IMAGE=True):
 
     # get checkpoint from SQL before loading next
     # Load Configurations and Init clients
@@ -45,7 +47,10 @@ def etl_flow(BATCHSIZE=32, CONFIG_PATH='test_config.yaml', MODEL_CONFIG_PATH = '
     # --------------------------------
     # Extract
     # --------------------------------
-    df_ckp = get_checkpoint(conn=conn, request_batch_size=BATCHSIZE)
+    df_ckp = get_checkpoint(
+        conn=conn,
+        request_batch_size=BATCHSIZE
+    )
 
     print(f'Processing {df_ckp.shape[0]} datapoints.')
 
@@ -56,18 +61,26 @@ def etl_flow(BATCHSIZE=32, CONFIG_PATH='test_config.yaml', MODEL_CONFIG_PATH = '
         n_threads=8,
     )
     # --------------------------------
-    # Transform
+    # Transform and Load
     # --------------------------------
+    # Inserts model config if not exists
+    db_insert_model_config(
+        conn=conn,
+        model_config=model_config
+    )
+
     flower_predictions, pollinator_predictions = model_predict(
         data=df_ckp,
         cfg=model_config
     )
-    # Insert image results and get back result_ids 
+    # Insert image results and get back result_ids
     result_ids = db_insert_image_results(
         conn=conn,
         data=df_ckp,
-        model_config=model_config
+        model_config=model_config,
+        allow_multiple_results=MULTI_RESULTS_FOR_IMAGE
     )
+    print(result_ids)
     df_ckp['result_id'] = result_ids
 
     if IS_TEST:
@@ -79,36 +92,39 @@ def etl_flow(BATCHSIZE=32, CONFIG_PATH='test_config.yaml', MODEL_CONFIG_PATH = '
         with open('pollinator_predictions.json', 'w') as json_file:
             json.dump(pollinator_predictions, json_file)
 
+    # Insert image_results
+
+    # Flower predictions pre-processing and ingestion
     flower_predictions = process_flower_predictions(
         flower_predictions=flower_predictions,
         result_ids=df_ckp,
         model_config=model_config
     )
+    flower_ids = db_insert_flower_predictions(
+        conn=conn,
+        data=flower_predictions
+    )
+    # Append IDs to flower predictions
+    flower_predictions = pd.concat(
+        [flower_predictions, pd.Series(flower_ids)],
+        axis=1
+    )
+    flower_predictions = flower_predictions.rename(
+        columns={0: 'flower_id'}
+    )
 
-
-
-    """pollinator_predictions = process_pollinator_predictions(
+    # Pollinator predictions pre-processing and ingestion
+    pollinator_predictions = process_pollinator_predictions(
         pollinator_predictions=pollinator_predictions,
-        result_ids=result_ids
-    )"""
-    # --------------------------------
-    # Load
-    # --------------------------------
+        flower_predictions=flower_predictions
+    )
+    db_insert_pollinator_predictions(
+        conn=conn,
+        data=pollinator_predictions
+    )
 
-    # Inserts model config if not exists
-    db_insert_model_config(conn=conn, model_config=model_config) 
-
-
-
-
-
-    # --------------------------------
-    # Update
-    # --------------------------------
-    
     conn.close()
 
 
-    
 if __name__ == '__main__':
     etl_flow()
