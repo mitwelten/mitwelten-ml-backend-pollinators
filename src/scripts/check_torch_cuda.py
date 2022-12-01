4225c7c4df7638650aa9c1c31cf8491810a5fa89
Simon.Staehli <simon.a.staehli@gmail.com>
2022-12-01 11:13:45 +0100

    update

--- src/scripts/check_torch_cuda.py
+++ src/scripts/check_torch_cuda.py
@@ -1,17 +1,33 @@
+import sys
+import time
+
 import torch.cuda as cuda
 import pandas as pd
 
+
 if __name__ == '__main__':
+    for i in range(6):
+        sys.stdout.write('Checking for CUDA devices ' + i*'.' + '\r')
+        time.sleep(.3)
+
     print('Checking for CUDA devices ...')
+    
     print(f'CUDA Device available: [{cuda.is_available()}]')
     if cuda.is_available():
-        device = cuda.get_device_properties(0)
-        df = pd.DataFrame(data={
-            'Name': [device.name],
-            'Major': [device.major],
-            'Minor': [device.minor],
-            'Memory': [device.total_memory / (1000 * 1000)],
-            'Multi Processor Count': [device.multi_processor_count]
-        })
+        df = None
+        for i in range(cuda.device_count()):    
+            device = cuda.get_device_properties(i)
+            df_ = pd.DataFrame(data={
+                'Name': [device.name],
+                'Major': [device.major],
+                'Minor': [device.minor],
+                'Memory (MB)': [device.total_memory / (1000 * 1000)],
+                'Multi Processor Count': [device.multi_processor_count]
+            })
+            if df is None:
+                df = df_.copy()
+            else:
+                df = pd.concat([df, df_], axis=0)
+        
         print(df.to_markdown())
         
