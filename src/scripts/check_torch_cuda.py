f1d0d561fb80ef26f7dac14f21370124c2ab2974
Simon.Staehli <simon.a.staehli@gmail.com>
2022-12-01 10:58:06 +0100

    add script to check for cuda device

---
+++ src/scripts/check_torch_cuda.py
@@ -0,0 +1,17 @@
+import torch.cuda as cuda
+import pandas as pd
+
+if __name__ == '__main__':
+    print('Checking for CUDA devices ...')
+    print(f'CUDA Device available: [{cuda.is_available()}]')
+    if cuda.is_available():
+        device = cuda.get_device_properties(0)
+        df = pd.DataFrame(data={
+            'Name': [device.name],
+            'Major': [device.major],
+            'Minor': [device.minor],
+            'Memory': [device.total_memory / (1000 * 1000)],
+            'Multi Processor Count': [device.multi_processor_count]
+        })
+        print(df.T.to_markdown())
+        
